49050526da6b52147c397f689a5abb4825acca29
givka <givka.dev@gmail.com>
2019-01-02 18:48:05 +0100

    add more exports

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { Scraper } from './lib/scraper';
+export { Message } from './lib/message';
+export { ProxyFetcher } from './lib/proxy-fetcher';
+export { Serie } from './lib/serie';
+export { Album } from './lib/album';
