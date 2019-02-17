8a642cd5656c4466578b9c6ca047933c9f3de0d7
givka <givka.dev@gmail.com>
2019-02-17 15:16:55 +0100

    export author class

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { Scraper } from './lib/scraper';
 export { ProxyFetcher } from './lib/proxy-fetcher';
 export { Serie } from './lib/serie';
 export { Album } from './lib/album';
+export { Author } from './lib/author';
